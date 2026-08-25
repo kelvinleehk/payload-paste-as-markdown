70906ea2415e7b079923d66f71fb659b49a8c6bf
Kelvin Lee <kelvinlee@onioncreative.com>
2026-08-25 22:56:00 +0800

    chore: rename package to payload-paste-as-markdown

--- src/feature/feature.server.ts
+++ src/feature/feature.server.ts
@@ -2,7 +2,7 @@ import { createServerFeature } from '@payloadcms/richtext-lexical'
 
 export const MarkdownPasteFeature = createServerFeature({
   feature: () => ({
-    ClientFeature: 'paste-as-markdown/client#MarkdownPasteFeatureClient',
+    ClientFeature: 'payload-paste-as-markdown/client#MarkdownPasteFeatureClient',
   }),
   key: 'markdownPaste',
 })
